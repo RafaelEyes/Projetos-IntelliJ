0668ad274e9ed8836ada4ee93ed9576f5f4274b4
Rafael Martins <fael142a@gmail.com>
2025-10-11 19:46:38 -0300

    TABUADAdo5

--- EXERCICIOS (FEITOS)/EXERCÍCIOS 3.1/TABUADAdo5.java
+++ EXERCICIOS (FEITOS)/EXERCÍCIOS 3.1/TABUADAdo5.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int numero = 0; numero <= 10; numero++) {
-            System.out.println("5x" + numero + "=" + (5 * 10));
+            System.out.println("5x" + numero + "=" + (5 * numero));
         }
     }
 }
